fb886f7d4b51cf490c14af4b52a3e3680c188ced
qiujiangkun <qiujiangkun@foxmail.com>
2019-03-29 12:34:28 +0800

    exception process

--- get_bing_dict.py
+++ get_bing_dict.py
@@ -3,18 +3,21 @@ import requests as rq
 import sys
 from bs4 import BeautifulSoup
 
-words = input() if len(sys.argv) <= 1 else ' '.join(sys.argv[1:])
-url = 'https://cn.bing.com/dict/search'
-r = rq.get(url, params={'q': words} )
-soup = BeautifulSoup(r.text, 'lxml')
+try:
+    words = input() if len(sys.argv) <= 1 else ' '.join(sys.argv[1:])
+    url = 'https://cn.bing.com/dict/search'
+    r = rq.get(url, params={'q': words} )
+    soup = BeautifulSoup(r.text, 'lxml')
 
-with open('/'.join(sys.argv[0].split('/')[:-1]) + '/style.css', 'r') as f:
-    s = f.read()
-soup.find_all('style')[0].string = s
-#soup.html.body.header.decompose()
-soup.html.body.footer.decompose()
-soup.find_all(class_='sidebar')[0].decompose()
+    with open('/'.join(sys.argv[0].split('/')[:-1]) + '/style.css', 'r') as f:
+        s = f.read()
+    soup.find_all('style')[0].string = s
+    #soup.html.body.header.decompose()
+    soup.html.body.footer.decompose()
+    soup.find_all(class_='sidebar')[0].decompose()
 
-print(soup)
+    print(soup)
 
+except Exception as ex:
+    print("<h2 style='color: red;'>找不到单词:</h2>" + words)
 
